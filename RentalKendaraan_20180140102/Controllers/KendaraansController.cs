515e1ee62728e35bb300951a37f9edb539185236
Mochammad Reza Rafli Zamzam Alfarizi <rezarafli9668@gmail.com>
2020-10-31 12:16:18 +0700

    Exercise 1

---
+++ RentalKendaraan_20180140102/Controllers/KendaraansController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentalKendaraan_20180140102.Models;
+
+namespace RentalKendaraan_20180140102.Controllers
+{
+    public class KendaraansController : Controller
+    {
+        private readonly rental_kendaraanContext _context;
+
+        public KendaraansController(rental_kendaraanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Kendaraans
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Kendaraan.ToListAsync());
+        }
+
+        // GET: Kendaraans/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kendaraan = await _context.Kendaraan
+                .FirstOrDefaultAsync(m => m.IdKendaraan == id);
+            if (kendaraan == null)
+            {
+                return NotFound();
+            }
+
+            return View(kendaraan);
+        }
+
+        // GET: Kendaraans/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kendaraans/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdKendaraan,NamaKendaraan,NoPolisi,NoStnk,IdJenisKendaraan,Ketersediaan")] Kendaraan kendaraan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(kendaraan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kendaraan);
+        }
+
+        // GET: Kendaraans/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kendaraan = await _context.Kendaraan.FindAsync(id);
+            if (kendaraan == null)
+            {
+                return NotFound();
+            }
+            return View(kendaraan);
+        }
+
+        // POST: Kendaraans/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdKendaraan,NamaKendaraan,NoPolisi,NoStnk,IdJenisKendaraan,Ketersediaan")] Kendaraan kendaraan)
+        {
+            if (id != kendaraan.IdKendaraan)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kendaraan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KendaraanExists(kendaraan.IdKendaraan))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kendaraan);
+        }
+
+        // GET: Kendaraans/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kendaraan = await _context.Kendaraan
+                .FirstOrDefaultAsync(m => m.IdKendaraan == id);
+            if (kendaraan == null)
+            {
+                return NotFound();
+            }
+
+            return View(kendaraan);
+        }
+
+        // POST: Kendaraans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var kendaraan = await _context.Kendaraan.FindAsync(id);
+            _context.Kendaraan.Remove(kendaraan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KendaraanExists(int id)
+        {
+            return _context.Kendaraan.Any(e => e.IdKendaraan == id);
+        }
+    }
+}
---
+++ RentalKendaraan_20180140102/Models/rental_kendaraanContext.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RentalKendaraan_20180140102.Models
+{
+    public partial class rental_kendaraanContext : DbContext
+    {
+        public rental_kendaraanContext()
+        {
+        }
+
+        public rental_kendaraanContext(DbContextOptions<rental_kendaraanContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Gender> Gender { get; set; }
+        public virtual DbSet<Jaminan> Jaminan { get; set; }
+        public virtual DbSet<JenisKendaraan> JenisKendaraan { get; set; }
+        public virtual DbSet<Kendaraan> Kendaraan { get; set; }
+        public virtual DbSet<KondisiKendaraan> KondisiKendaraan { get; set; }
+        public virtual DbSet<Peminjaman> Peminjaman { get; set; }
+        public virtual DbSet<Pengembalian> Pengembalian { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.IdCustomer);
+
+                entity.Property(e => e.IdCustomer)
+                    .HasColumnName("ID_Customer")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Alamat)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdGender).HasColumnName("ID_Gender");
+
+                entity.Property(e => e.NamaCustomer)
+                    .HasColumnName("Nama_Customer")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nik)
+                    .HasColumnName("NIK")
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoHp)
+                    .HasColumnName("No_HP")
+                    .HasMaxLength(13)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Gender>(entity =>
+            {
+                entity.HasKey(e => e.IdGender);
+
+                entity.Property(e => e.IdGender).HasColumnName("ID_Gender");
+
+                entity.Property(e => e.NamaGender)
+                    .HasColumnName("Nama_Gender")
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Jaminan>(entity =>
+            {
+                entity.HasKey(e => e.IdJaminan);
+
+                entity.Property(e => e.IdJaminan).HasColumnName("ID_Jaminan");
+
+                entity.Property(e => e.NamaJaminan)
+                    .HasColumnName("Nama_Jaminan")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<JenisKendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdJenisKendaraan);
+
+                entity.ToTable("Jenis_Kendaraan");
+
+                entity.Property(e => e.IdJenisKendaraan).HasColumnName("ID_Jenis_Kendaraan");
+
+                entity.Property(e => e.NamaJenisKendaraan)
+                    .HasColumnName("Nama_Jenis_Kendaraan")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Kendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdKendaraan);
+
+                entity.Property(e => e.IdKendaraan).HasColumnName("ID_Kendaraan");
+
+                entity.Property(e => e.IdJenisKendaraan).HasColumnName("ID_Jenis_Kendaraan");
+
+                entity.Property(e => e.Ketersediaan)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NamaKendaraan)
+                    .HasColumnName("Nama_Kendaraan")
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoPolisi)
+                    .HasColumnName("No_Polisi")
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoStnk)
+                    .HasColumnName("No_STNK")
+                    .HasMaxLength(8)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<KondisiKendaraan>(entity =>
+            {
+                entity.HasKey(e => e.IdKondisi);
+
+                entity.ToTable("Kondisi_Kendaraan");
+
+                entity.Property(e => e.IdKondisi).HasColumnName("ID_Kondisi");
+
+                entity.Property(e => e.NamaKondisi)
+                    .HasColumnName("Nama_Kondisi")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Peminjaman>(entity =>
+            {
+                entity.HasKey(e => e.IdPeminjaman);
+
+                entity.Property(e => e.IdPeminjaman).HasColumnName("ID_Peminjaman");
+
+                entity.Property(e => e.IdCustomer).HasColumnName("ID_Customer");
+
+                entity.Property(e => e.IdJaminan).HasColumnName("ID_Jaminan");
+
+                entity.Property(e => e.IdKendaraan).HasColumnName("ID_Kendaraan");
+
+                entity.Property(e => e.TglPeminjaman)
+                    .HasColumnName("tgl_Peminjaman")
+                    .HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<Pengembalian>(entity =>
+            {
+                entity.HasKey(e => e.IdPengembalian);
+
+                entity.Property(e => e.IdPengembalian).HasColumnName("ID_Pengembalian");
+
+                entity.Property(e => e.IdKondisi).HasColumnName("ID_Kondisi");
+
+                entity.Property(e => e.IdPeminjaman).HasColumnName("ID_Peminjaman");
+
+                entity.Property(e => e.TglPengembalian)
+                    .HasColumnName("tgl_Pengembalian")
+                    .HasColumnType("datetime");
+            });
+        }
+    }
+}
